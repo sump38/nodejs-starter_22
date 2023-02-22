ed05c9e8ecd1cc8f0d726e21052a1ed7704d1e09
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-02-22 20:31:21 +0200

    express basic server

--- server.js
+++ server.js
@@ -1,77 +1,9 @@
-const http = require('http');
+const express = require('express');
 
-const js = `
-    alert('my Script is working');
-`
+const app = express();
 
-const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Website!</title>
-        <script src="script.js"></script>
-    </head>
-    <body>
-        Hello world
-    </body>
-    </html>
-`;
-
-
-const runSwitchFunction = (state) => {
-    switch(state) {
-        case ('Done'): {
-            console.log('done!');
-            break;
-        }
-         case ('Error'): {
-             console.log('error occured');
-             break;
-        } 
-         case 'Loading': {
-             console.log('still loading');
-             break;
-        } 
-         default: {
-             console.log('no setting occured');
-             break; 
-         }
-     }
-}
-
-
-
-const server = http.createServer((request, response) => {
-    console.log(request.url);
-    response.statusCode = 200;
-
-    switch(request.url) {
-        case ('/favicon.ico'): {
-            response.statusCode = 404;
-            break;
-        }
-        case ('/hello'): {
-            response.setHeader('content-type', 'text/html');
-            response.write(html);
-            break;
-        }
-        case ('/script.js'): {
-            response.write(js);
-            break;
-        }
-        default: {
-            const name = request.url.toString().replace('/',''); 
-            response.setHeader('content-type', 'text/html');
-            response.write(`<html><body>Hello ${name}</body></html>`);
-            break;
-        }
-    }
-
-    response.end();
-
+app.use('/',(req, res) => {
+    res.send('<html><body>hello world</body></html>');
 });
 
-server.listen(999);
+app.listen(999);
