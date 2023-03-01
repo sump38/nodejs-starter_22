3f91c635ff8a470fed75787b1c6d0e084b940da8
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-03-01 17:25:54 +0200

    added basic routes

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use('/',(req, res) => {
-    res.send('<html><body>hello world</body></html>');
+app.use('/home', (req,res) => {
+    res.send('<html><body>Hello homepage</body></html>');
+})
+
+app.use('/redirect',(req, res) => {
+    res.redirect('https://www.nodejs.com');
+    // res.send('<html><body>hello world</body></html>');
 });
+
+app.use('/', (req, res) => {
+    res.status(404).send('NOT FOUND');
+})
+
 
 app.listen(999);
