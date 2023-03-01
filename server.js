ff4cbba6cdcfbfb3ceb64dda7c697d27515fe33c
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-03-01 20:13:58 +0200

    started products

--- server.js
+++ server.js
@@ -1,18 +1,56 @@
 const express = require('express');
+const path = require('path');
+const static = express.static('public');
+
+const products = [
+    {
+      "name" : "Black Coffee",
+      "price" : 22,
+      "description": "A very important drink for programmers"
+    },
+    {
+      "name" : "Black Coffee 2",
+      "price" : 22,
+      "description": "A very important drink for programmers"
+    },
+    {
+      "name" : "Black Coffee 3",
+      "price" : 22,
+      "description": "A very important drink for programmers"
+    }
+  ]
+
+
+let views = 0;
+
 
 const app = express();
 
-app.use('/home', (req,res) => {
-    res.send('<html><body>Hello homepage</body></html>');
-})
+app.use(static);
+
+app.use('/', (req, res, next) => {
+    // console.log('views:' + ++views);
+    console.log(req.url);
+    next();
+    // console.log('after next');
+});
+
+app.use('/home', (req,res,next) => {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, 'files', 'index.html'));
+});
 
 app.use('/redirect',(req, res) => {
     res.redirect('https://www.nodejs.com');
     // res.send('<html><body>hello world</body></html>');
 });
 
+app.use('/products', (req, res) => {
+    res.json(products);
+})
+
 app.use('/', (req, res) => {
-    res.status(404).send('NOT FOUND');
+    res.status(404).sendFile(path.join(__dirname, 'files', '404.html'));
 })
 
 
