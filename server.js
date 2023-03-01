a76ec613ea91e88a1fe71f01a32b65c167dad179
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-03-01 16:49:46 +0200

    json endpoints

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+//cors missing
+
+const products = [
+      {
+        "name" : "Black Coffee",
+        "price" : 22,
+        "description": "A very important drink for programmers"
+      },
+      {
+        "name" : "Black Coffee 2",
+        "price" : 22,
+        "description": "A very important drink for programmers"
+      },
+      {
+        "name" : "Black Coffee 3",
+        "price" : 22,
+        "description": "A very important drink for programmers"
+      }
+];
+
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/products', (req,res, next) => {
+    res.json(products);
+})
+
+app.post('/api/additem', (req, res, next) => {
+    itemToAdd = req.body;
+    //test if actual product!
+    products.push(itemToAdd);
+    res.json("success");
+});
 
 app.use('/',(req, res) => {
     res.send('<html><body>hello world</body></html>');
